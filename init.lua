6871555a725d162e64ad085af52ddb6d02284ba1
Henry Jotnar <gribbonh@hotmail.com>
2024-09-25 21:17:17 +0100

    fix sign column looking a bit off

--- init.lua
+++ init.lua
@@ -830,6 +830,11 @@ require('lazy').setup({
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'ellisonleao/gruvbox.nvim',
+    opts = {
+      overrides = {
+        SignColumn = {bg = '#282828'},
+      },
+    },
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
